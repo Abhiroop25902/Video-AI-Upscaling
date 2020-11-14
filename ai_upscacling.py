ad1b10f4c23b56f3d2566719123c6d824ba071fc
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2020-11-15 03:06:33 +0530

    Update ai_upscacling.py

--- ai_upscacling.py
+++ ai_upscacling.py
@@ -4,9 +4,14 @@ import numpy as np
 
 cap = cv.VideoCapture("network.mp4")
 
+#these parameters depends on video and should be changed
+output_video_width = 3*500
+output_video_height = 3*280
+video_total_frame = 121
+
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'mp4v')
-out = cv.VideoWriter('output.mp4',fourcc, 20.0, (1500,840))
+out = cv.VideoWriter('output.mp4',fourcc, 20.0,(output_video_width,output_video_height))
 
 # Create an SR object
 sr = dnn_superres.DnnSuperResImpl_create()
@@ -18,10 +23,13 @@ sr.readModel(path)
 # Set the desired model and scale to get correct pre- and post-processing
 sr.setModel("edsr", 3)
 
+#setting dnn to use CUDA
 sr.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
 sr.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
 
+#just for verbose in terminal
 frame = 0
+
 
 while True:
     # Read image
@@ -34,10 +42,11 @@ while True:
     result = sr.upsample(image)
     frame = frame + 1
 
-    print(f"{frame}/121 done")
+    #verbose in terminal
+    print(f"{frame}/{video_total_frame} done")
 
     out.write(result)
 
-
+#release capture anf
 cap.release()
 out.release()
